c7af5252c0b3737a7bae278864e048a4ecd9e062
Eduardo Oliva <eduardok07@hotmail.com>
2020-04-17 21:58:57 -0300

    Adding to remote repository

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
+
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    handleList();
+  }, [])
+
+  async function handleList() {
+    const response = await api.get('repositories');
+
+    const repositories = response.data;
+
+    setRepositories(repositories);
+  }
+
+  async function handleAddRepository() {
+    const response = await api.post('/repositories', {
+      title: `Novo projeto ${Date.now()}`,
+      url: 'https://github.com/EduardoKO',
+      techs: ['ReactJS', 'React Native', 'NodeJS']
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+  }
+
+  async function handleRemoveRepository(id) {
+    try {
+      await api.delete(`repositories/${id}`);
+    
+      setRepositories(repositories.filter(repository => repository.id !== id));
+
+    }catch(err) {
+      alert(err);
+    }
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repository =>
+          <li key={repository.id}>
+            {repository.title}
+
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+        </li>)}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
